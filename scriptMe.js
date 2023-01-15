932d3887239e174573675d49475f352d50c3becb
MiNareee <109744081+MiNareee@users.noreply.github.com>
2023-01-15 15:50:26 +0300

    Create scriptMe.js

---
+++ scriptMe.js
@@ -0,0 +1,22 @@
+ymaps.ready(function(){;
+  let mymap=new ymaps.Map('map',{
+    center:[55.75366489822893,37.626052494342886],
+    zoom:17,
+    controls:['routePanelControl']
+  });
+  let control= mymap.controls.get('routePanelControl');
+  let city='Москва';
+  let location =ymaps.geolocation.get();
+      location.then(function(res){
+      letlocationText = res.geolocation.get(0).properties.get('text');
+      console.log(locationText)
+      });
+                       
+  control.routePanel.state.set({
+    type: 'masstransit',
+    fromEnabled: false,
+    from: `${city}, Петра Романова 4,
+    toEnabled: true,
+    to: `${city}, 5-я Кожуховская 6,
+  });
+ })
