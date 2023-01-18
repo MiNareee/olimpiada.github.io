50b0cd8a44573cd5d3274d555398426eeff43b6d
MiNareee <109744081+MiNareee@users.noreply.github.com>
2023-01-18 09:12:39 +0300

    Update 3pains.js

--- 3pains.js
+++ 3pains.js
@@ -26,16 +26,4 @@ ymaps.ready(function () {
 
     // Добавление маршрута на карту.
     myMap.geoObjects.add(multiRoute);
-    multiRoute.model.events.add('requestsuccess', function() {
-    // Получение ссылки на активный маршрут.
-    let activeRoute = multiRoute.getActiveRoute();
-    // Вывод информации о маршруте.
-      console.log("Длина: " + activeRoute.properties.get("distance").text);
-      console.log("Время прохождения: " + activeRoute.properties.get("duration").text);
-    // Для автомобильных маршрутов можно вывести 
-    // информацию о перекрытых участках.
-      if (activeRoute.properties.get("blocked")) {
-        console.log("На маршруте имеются участки с перекрытыми дорогами.");
-    }
-  });
 });   
