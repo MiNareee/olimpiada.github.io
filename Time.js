98a61a1caf7c43f5503336823abd48d80dc2b484
MiNareee <109744081+MiNareee@users.noreply.github.com>
2023-01-22 20:58:36 +0300

    Delete Time.js

--- Time.js
+++
@@ -1,4 +0,0 @@
-setInterval(function() {
-  var currentTime = new Date().toLocaleTimeString('ru-RU', { timeZone: 'Europe/Moscow' });
-  document.getElementById('currentTime').innerHTML = currentTime;
-}, 1000);
