66667be386b6ca5d01cdbcf58d7bcf001ea3cf32
MiNareee <109744081+MiNareee@users.noreply.github.com>
2023-02-20 10:54:49 +0300

    Update pok.js

--- JavaScript/pok.js
+++ JavaScript/pok.js
@@ -66,9 +66,9 @@ function alerted(){
 	var checked = [];
 	$('input:checkbox:checked').each(function() {
 	checked.push($(this).val());
-	});
-	var fs=[]
+	})
 	console.log(checked)
+	var fs=[]
 	for(let i=0;i<checked.length;i++){
 		let t = checked[i]-1
 		fs.push(s[t])
